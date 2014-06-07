5115078595184675f01ab0371e3953ced1e7656e
Linshuanity <edison4987.cs00@nctu.edu.tw>
2014-06-07 02:30:03 -0700

    Backup

--- codeGeneration.c
+++ codeGeneration.c
@@ -2,12 +2,48 @@
 #include <string.h>
 #include "header.h"
 
+#define FRAMESIZE 64;
+
 char *function_name;
 FILE *output;
 
-
 int reg[32] = {0};
 int freg[32] = {0};
+int ARoffset = -4;
+// Global counter
+int exit_counter = 0;
+int else_counter = 0;
+
+// Register related function
+int get_reg();
+int get_freg();
+int get_offset(SymbolTableEntry* entry);
+void free_reg(int free_id);
+void free_freg(int free_id);
+
+void gen_code(AST_NODE *proj);
+void gen_global_decl(AST_NODE *global_decl);
+void gen_decl_list(AST_NODE *decl_list);
+void gen_var_decl(AST_NODE *nodePtr);
+void gen_func_type_empty(AST_NODE *func_head);
+void gen_block(AST_NODE *block);
+void gen_stmt_list(AST_NODE *stmt_ptr);
+void gen_head(char* name);
+
+// function declaration
+void gen_prologue(char* func_name);
+void gen_epilogue(char* name);
+void gen_AR_offset();
+void fill_local_var_offset(int offset, SymbolTableEntry* entry);
+
+// stmt generation
+void gen_assign_stmt();
+void gen_if_stmt();
+void gen_for_stmt();
+void gen_return_stmt();
+void gen_func_call_stmt();
+
+void fill_place(int place, AST_NODE* node);
 
 int get_reg()
 {
@@ -36,6 +72,11 @@ int get_freg()
 	return -1;
 }
 
+int get_offset(SymbolTableEntry* entry)
+{
+	return entry->offset;
+}
+
 void free_reg(int free_id)
 {
 	reg[free_id] = 0;
@@ -48,21 +89,16 @@ void free_freg(int free_id)
 
 void gen_code(AST_NODE *proj)
 {
-	AST_Node *nodePtr;
+	AST_NODE *nodePtr;
 	nodePtr = proj->child;
 	output = fopen("output.s", "w");
 
-	//TODO: 下面兩行可以刪掉	
-	if(nodePtr == NULL)
-		return;
-	fprintf(output, ".data"\n);
 
 	while(nodePtr != NULL){
 		gen_global_decl(nodePtr);
 		nodePtr = nodePtr->rightSibling;
 	}
 	
-	// dunp;
 	fclose(output);
 }
 
@@ -84,9 +120,10 @@ void gen_global_decl(AST_NODE *global_decl)
 
 void gen_decl_list(AST_NODE *decl_list)
 {
+	fprintf(output, ".data"\n);
 	while(decl_list != NULL){
 		if(decl_list->semantic_value.declSemanticValue.kind == VARIABLE_DECL){
-			gen_var_decl(decl_list);
+				gen_var_decl(decl_list);
 		}
 		else{
 			printf("Declaration node type unhandled\n");
@@ -94,6 +131,7 @@ void gen_decl_list(AST_NODE *decl_list)
 		decl_list = decl_list->rightSibling;
 	}
 }
+
 
 void gen_var_decl(AST_NODE *nodePtr)
 {
@@ -122,8 +160,10 @@ void gen_var_decl(AST_NODE *nodePtr)
 
 void gen_func_type_empty(AST_NODE *func_head)
 {	
+	int ARoffset = -4;
 	AST_NODE *nodePtr = func_head->child->rightSibling;
 	char* func_name = nodePtr->semantic_value.const1->sc;
+//	Are these needed ?
 	if(strcmp("read", func_name) == 0){
 		// gen_read();
 		return;
@@ -132,11 +172,12 @@ void gen_func_type_empty(AST_NODE *func_head)
 		// gen_write();
 		return;
 	}
-	
-	gen_text(func_name);
+//	
+	gen_head(func_name);
 	gen_prologue(func_name);
-	// ARoffset = -4;
+	fprintf(output, "_begin_%s:\n", func_name);
 	gen_block(nodePtr->rightSibling);
+	fprintf(output, "_end_%s:\n", func_name);
 	gen_epilogue(func_name);
 }
 
@@ -167,46 +208,47 @@ void gen_stmt_list(AST_NODE *stmt_ptr)
 }
 
 
-void gen_text(char* name)
+void gen_head(char* name)
 {
 	fprintf(output, ".text\n");
 	fprintf(output, "%s:\n", name);
 }
 
 
-void gen_stmt()
+void gen_stmt(AST_NODE* node)
 {
-
-}
-
-
-void gen_global_decl(AST_NODE *nodePtr)
-{
-	if(nodePtr->semantic_value.declSemanticValue->kind == VARIABLE_DECL){
-		fprintf(output, ".data\n");
-		AST_NODE *temp = nodePtr->child->rightSibling;
-		while(nodePtr != NULL){
-			if(temp->nodeType != IDENTIFIER_NODE)
-				printf("Expected to be ID_NODE\n");
-			switch(temp->dataType){
-				case NORMAL_ID:
-					//gen_global_decl(temp);
-					break;
-				case ARRAY_ID:
-					//gen_global_array_decl(temp);
-					break;
-				case WITH_INIT_ID:
-					//gen_global_init_decl(temp);
-					break;
-			}
-			temp = temp->rightSibling;
+	if(stmtNode->nodeType == NUL_NODE){
+		return;
+	}
+	else if(stmtNode->nodeType == BLOCK_NODE){
+		gen_block(node);
+	}
+	else{
+		switch(node->semantic_value.stmtSemanticValue.kind){
+			case IF_STMT:
+				gen_if_stmt();
+				break;
+			case FOR_STMT:
+				gen_for_stmt();
+				break;
+			case ASSIGN_STMT:
+				gen_assign_stmt();
+				break;
+			case FUNCTION_CALL_STMT:
+				gen_func_call_stmt();
+				break;
+			case RETURN_STMT:
+				gen_return_stmt();
+				break;
+			default:
+				printf("Unhandled STMT type\n");
 		}
 	}
-	else
-		printf("Should be VARIABLE_DECL type\n");
 }
 
-void gen_prologue(char* func_name) {
+
+void gen_prologue(char* func_name)
+{
 	fprintf(output, "# prologue sequence\n");
 	fprintf(output, "sw  $ra, 0($sp)\n");    //存return address
 	fprintf(output, "sw  $fp, -4($sp)\n");   //存現在的fp
@@ -236,7 +278,8 @@ void gen_prologue(char* func_name) {
 }
 
 
-void gen_epilogue(char* name) {
+void gen_epilogue(char* name)
+{
 	fprintf(output, "# epilogue sequence\n");
 	fprintf(output, "_end_%s:\n", name);
 	fprintf(output, "lw  $8,  64($sp)\n");   //$t0   
@@ -267,4 +310,98 @@ void gen_epilogue(char* name) {
 	} else { 
 		fprintf(output, "jr  $ra\n"); 
 	}
+	
+	fprintf(output, "\n.data\n");
+	fprintf("\t_framsize_%s: .word %d\n\n", name, FRAMESIZE - ARoffset);
+}
+
+
+void gen_AR_offset()
+{
+	
+}
+
+void fill_local_var_offset(int offset, SymbolTableEntry* entry)
+{
+	entry->offset = offset;
+}
+
+void gen_assign_stmt();
+void gen_if_stmt(AST_NODE* node)
+{
+	AST_NODE* test_expr = node->child;
+	AST_NODE* if_stmt = test_expr->rightSibling;
+	AST_NODE* else_stmt = if_stmt->rightSibling;
+
+	if(else_stmt != NULL){ // if-else case
+		fprintf(output, "\tbeqz\t$%d, Lelse_%d\n", test_expr->place, else_counter);
+		gen_stmt(if_stmt);
+		fprintf(output, "\tj Lexit_%d\n", exit_counter);
+
+		fprintf(output, "Lelse_%d\n:", else_counter);
+		gen_stmt(else_stmt);
+
+		fprintf(output, "Lexit_%d\n:", exit_counter);
+	}
+	else{
+		fprintf(output, "\tbeqz\t$%d, Lexit_%d", test_expr->place, exit_counter);
+		gen_stmt(if_stmt);
+
+		fprintf(output, "Lexit_:%d\n", exit_counter);
+	}
+	free_reg(test_expr->place);
+	else_counter++;
+	exit_counter++;		
+}
+void gen_for_stmt(AST_NODE* node)
+{
+	AST_NODE* assign_expr_1 = node->child;
+	AST_NODE* relop_expr = assign_expr->rightSibling;
+	AST_NODE* assign_expr_2 = relop_expr->rightSibling;
+	AST_NODE* statement = assign_expr2->rightSibling;
+
+	gen_assign_expr_list(assign_expr);
+
+	fprintf(output, "Test_%d:\n", test_counter);
+	gen_relop_expr_list(relop_expr);
+	fprintf(output, "\tbeqz\t$%d, Lexit_%d\n", relop_expr->place, exit_counter);
+	fprintf(output, "\tj Body_%d\n", body_counter);
+
+	fprintf(output, "Inc_%d:\n", inc_counter);
+	gen_assign_expr_list(assign_expr_2);
+	fprintf(output, "\tj TEST_%d\n", test_counter);
+
+	fprintf(output, "Body_%d:\n", body_counter);
+	gen_stmt(statement);
+	fprintf(output, "\tj Inc\n", Inc_counter);
+
+	fprintf(output, "Lexit_%d:\n", exit_counter);
+
+	free_reg(relop_expr->place);
+	test_counter++;
+	exit_counter++;
+	body_counter++;
+	Inc_counter++;	
+}
+void gen_return_stmt(AST_NODE* node)
+{
+	AST_NODE* relop_expr = node->child;
+	if();
+}
+void gen_func_call_stmt(AST_NODE* node)
+{
+	char* func_name = node->semantic_value.const1->sc;
+	if(strcmp(func_name, "write") == 0){
+		// gen_write;
+	}	
+	else if (strcmp(func_name, "read") == 0){
+		// gen_read;
+	}
+	else{
+		
+	}
+}
+void fill_place(int place, AST_NODE* node)
+{
+	node->place = place;
 }
