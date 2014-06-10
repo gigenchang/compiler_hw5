8ce3caac8b677816c31d7daaa59bfa0af78254f1
Linshuanity <edison4987.cs00@nctu.edu.tw>
2014-06-10 13:00:56 -0700

    test.c

--- output.s
+++ output.s
@@ -1,25 +1,69 @@
 .data
-_a: .word 0
-_b: .word 0
-_c: .word 0
-_d: .word 0
-_e: .word 0
-_f: .word 0
-_g: .word 0
-_h: .word 0
-_i: .word 0
-_j: .word 0
-_k: .word 0
-_l: .word 0
-_m: .word 0
-_n: .word 0
-_o: .word 0
-_p: .word 0
-_q: .word 0
-_r: .word 0
-_s: .word 0
-_t: .word 0
-_u: .word 0
+_ch: .space 96
+.text
+bbb:
+# prologue sequence
+	sw  $ra, 0($sp)
+	sw  $fp, -4($sp)
+	add $fp, $sp, -4
+	add $sp, $sp, -8
+	lw  $v0, _framesize_of_bbb
+	sub $sp, $sp, $v0
+	sw  $8,  64($sp)
+	sw  $9,  60($sp)
+	sw  $10, 56($sp)
+	sw  $11, 52($sp)
+	sw  $12, 48($sp)
+	sw  $13, 44($sp)
+	sw  $14, 40($sp)
+	sw  $15, 36($sp)
+	sw  $24, 32($sp)
+	sw  $25, 28($sp)
+	s.s  $f4, 24($sp)
+	s.s  $f6, 20($sp)
+	s.s  $f8, 16($sp)
+	s.s  $f10, 12($sp)
+	s.s  $f16, 8($sp)
+	s.s  $f18, 4($sp)
+_begin_bbb:
+	li.s  $f4, 4.000000
+	li.s  $f6, 3.000000
+	li.s  $f8, 2.000000
+	li   $v0, 6
+	syscall
+	mov.s $f10, $f0
+	add.s  $f26, $f8, $f10
+	s.s	$f26, -4($fp)
+	l.s	$f30, -4($fp)
+	add.s  $f8, $f6, $f30
+	add.s  $f6, $f4, $f8
+	mov.s	$f0, $f6
+_end_bbb:
+# epilogue sequence
+	lw  $8,  64($sp)
+	lw  $9,  60($sp)
+	lw  $10, 56($sp)
+	lw  $11, 52($sp)
+	lw  $12, 48($sp)
+	lw  $13, 44($sp)
+	lw  $14, 40($sp)
+	lw  $15, 36($sp)
+	lw  $24, 32($sp)
+	lw  $25, 28($sp)
+	l.s  $f4, 24($sp)
+	l.s  $f6, 20($sp)
+	l.s  $f8, 16($sp)
+	l.s  $f10, 12($sp)
+	l.s  $f16, 8($sp)
+	l.s  $f18, 4($sp)
+	lw   $ra, 4($fp)
+	add  $sp, $fp, 4
+	lw   $fp, 0($fp)
+	jr  $ra
+
+.data
+	_framesize_of_bbb: .word 72
+
 .text
 main:
 # prologue sequence
@@ -46,99 +90,42 @@ main:
 	s.s  $f16, 8($sp)
 	s.s  $f18, 4($sp)
 _begin_main:
-	li  $8, 1
-	sw  $8, _a
-	li  $8, 2
-	sw  $8, _b
-	li  $8, 3
-	sw  $8, _c
-	li  $8, 4
-	sw  $8, _d
-	li  $8, 5
-	sw  $8, _e
-	li  $8, 6
-	sw  $8, _f
+	jal  bbb
+	mov.s $f4, $f0
+	s.s  $f4, -8($fp)
 	li  $8, 7
-	sw  $8, _g
-	li  $8, 8
-	sw  $8, _h
-	li  $8, 9
-	sw  $8, _i
-	li  $8, 10
-	sw  $8, _j
-	li  $8, 11
-	sw  $8, _k
-	li  $8, 12
-	sw  $8, _l
-	li  $8, 13
-	sw  $8, _m
-	li  $8, 14
-	sw  $8, _n
-	li  $8, 15
-	sw  $8, _o
-	li  $8, 16
-	sw  $8, _p
-	li  $8, 17
-	sw  $8, _q
-	li  $8, 18
-	sw  $8, _r
-	li  $8, 19
-	sw  $8, _s
-	li  $8, 20
-	sw  $8, _t
-	li  $8, 21
-	sw  $8, _u
-	lw  $8, _a
-	lw  $9, _b
-	lw  $10, _c
-	lw  $11, _d
-	lw  $12, _e
-	lw  $13, _f
-	lw  $14, _g
-	lw  $15, _h
-	lw  $16, _i
-	lw  $17, _j
-	lw  $18, _k
-	lw  $19, _l
-	lw  $20, _m
-	lw  $21, _n
-	lw  $22, _o
-	lw  $23, _p
-	lw  $24, _q
-	sw	$24, -4($fp)
-	lw  $24, _r
-	sw	$24, -8($fp)
-	lw  $24, _s
-	sw	$24, -12($fp)
-	lw  $24, _t
-	sw	$24, -16($fp)
-	add  $24, $-12, $-16
-	sw	$24, -20($fp)
-	add  $24, $-8, $-20
-	sw	$24, -24($fp)
-	add  $24, $-4, $-24
-	sw	$24, -28($fp)
-	add  $24, $23, $-28
-	sw	$24, -32($fp)
-	add  $23, $22, $-32
-	add  $22, $21, $23
-	add  $21, $20, $22
-	add  $20, $19, $21
-	add  $19, $18, $20
-	add  $18, $17, $19
-	add  $17, $16, $18
-	add  $16, $15, $17
-	add  $15, $14, $16
-	add  $14, $13, $15
-	add  $13, $12, $14
-	add  $12, $11, $13
-	add  $11, $10, $12
-	add  $10, $9, $11
-	add  $9, $8, $10
-	sw  $9, _u
-	lw  $8, _u
+	li  $9, 0
+	li  $10, 1
+	add $9, $9, $10
+	li  $25, 3
+	mul $9, $9, $25
+	li  $11, 0
+	add $9, $9, $11
+	li  $25, 4
+	mul $9, $9, $25
+	li  $12, 2
+	add $9, $9, $12
+	mul $9, $9, 4
+	sw  $8, _ch+0($9)
+	li  $8,  0
+	li  $9, 1
+	add $8, $8, $9
+	li  $9, 3
+	mul $8, $8, $9
+	li  $9, 0
+	add $8, $8, $9
+	li  $9, 4
+	mul $8, $8, $9
+	li  $9, 2
+	add $8, $8, $9
+	mul $8, $8, 4
+	lw  $9, _ch+0($8)
 	li    $v0, 1
-	move  $a0, $8
+	move  $a0, $9
+	syscall
+	l.s  $f4, -8($fp)
+	li    $v0, 2
+	mov.s $f12, $f4
 	syscall
 .data
 str_1: .asciiz "\n"
@@ -171,6 +158,6 @@ _end_main:
 	syscall
 
 .data
-	_framesize_of_main: .word 100
+	_framesize_of_main: .word 76
 
 .data
--- pattern/test.c
+++ pattern/test.c
@@ -1,33 +1,16 @@
+int ch[2][3][4];
+
 float bbb()
 {
-	float a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u;
-	a = 1.0;
-	b = 2.0;
-	c = 3.0;
-	d = 4.0;
-	e = 5.0;
-	f = 6.0;
-	g = 7.0;
-	h = 8.0;
-	i = 9.0;
-	j = 10.0;
-	k = 11.0;
-	l = 12.0;
-	m = 13.0;
-	n = 14.0;
-	o = 15.0;
-	p = 16.0;
- 	q = 17.0;
-	r = 18.0;
-	s = 19.0;
-	t = 20.0;
-	return a + (b + (c + (d + (e + (f + (g + (h + (i + (j + (k + (l + (m + (n + (o + (p + (q + (r + (s + (t)))))))))))))))))));
+	return 4.0 + (3.0 + (2.0 + fread()));
 }
 
 int main() {
+
 	float b;
 	b = bbb();
-
+	ch[1][0][2] = 7;
+	write(ch[1][0][2]);
 	write(b);
 	write("\n");
 }
